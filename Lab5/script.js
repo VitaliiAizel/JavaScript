413743251112846ad6ea90a27b8419be7358d282
VitaliiAizel <91182229+VitaliiAizel@users.noreply.github.com>
2024-04-10 02:36:37 +0300

    Update script.js

--- Lab5/script.js
+++ Lab5/script.js
@@ -82,8 +82,6 @@ document.addEventListener("DOMContentLoaded", function () {
         document.getElementById('modal').style.display = 'block';
 
         const modalContent = document.getElementById('modalContent');
-        modalContent.innerHTML += '<p>Punktzahl: ${score}</p>' +
-        '<p>Möchtest du noch einmal spielen?</p>' +
-        '<p>Wenn ja, laden Sie die Seite neu.</p>';
+        modalContent.innerHTML += '<p>Punktzahl: ${score}</p>';
     }
 });
