0c43ef263011a4b98df7770b73cdc561988f0515
VitaliiAizel <91182229+VitaliiAizel@users.noreply.github.com>
2024-04-24 05:58:29 +0300

    Update script.js

--- Lab6/js/script.js
+++ Lab6/js/script.js
@@ -1,15 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
-
     const timeLeftElement = document.getElementById("time");
     const step = document.getElementById("step");
+    let gameOver = false;
     let steps = 0;
     let sec = 0;
     let min = 0;
     let timer;
     let target;
     document.getElementById("startButton").addEventListener("click", request);
-    document.getElementById("restartButton").addEventListener("click", restart); // Додано обробник подій для кнопки "restartButton"
-    
+    document.getElementById("restartButton").addEventListener("click", restart);
+
     function restart() {
         clearInterval(timer);
         document.getElementById("modalArea").style.display = "none";
@@ -17,6 +17,7 @@ document.addEventListener("DOMContentLoaded", function () {
         document.getElementById("gamezone").innerHTML = "";
         timeLeftElement.textContent = "0:00";
         step.textContent = 0;
+        gameOver = false;
         steps = 0;
         sec = 0;
         min = 0;
@@ -29,9 +30,9 @@ document.addEventListener("DOMContentLoaded", function () {
             if (rqst.readyState == XMLHttpRequest.DONE) {
                 if (rqst.status == 200) {
                     const response = JSON.parse(rqst.responseText);
-                    const randomIndex = Math.floor(Math.random() * response.length); 
-                    tiles = response[randomIndex].table; 
-                    target = response[randomIndex].target; 
+                    const randomIndex = Math.floor(Math.random() * response.length);
+                    tiles = response[randomIndex].table;
+                    target = response[randomIndex].target;
                     Start(tiles, target);
                 } else
                     console.error("Request failed with status:", rqst.status);
@@ -50,33 +51,36 @@ document.addEventListener("DOMContentLoaded", function () {
         let tdElements = document.querySelectorAll("#gamezone td");
         tdElements.forEach(td => {
             td.addEventListener("click", function () {
-                let id = td.id.split("").map(Number);
-                let allOff = true;
-                let i = id[0];
-                let j = id[1];
+                if (!gameOver) {
+                    let id = td.id.split("").map(Number);
+                    let allOff = true;
+                    let i = id[0];
+                    let j = id[1];
 
-                colorSwaping(td);
+                    colorSwaping(td);
 
-                if (i > 0) colorSwaping(document.getElementById((i - 1) + "" + j));
-                if (i < tiles.length - 1) colorSwaping(document.getElementById((i + 1) + "" + j));
+                    if (i > 0) colorSwaping(document.getElementById((i - 1) + "" + j));
+                    if (i < tiles.length - 1) colorSwaping(document.getElementById((i + 1) + "" + j));
 
-                if (j > 0) colorSwaping(document.getElementById(i + "" + (j - 1)));
-                if (j < tiles[0].length - 1) colorSwaping(document.getElementById(i + "" + (j + 1)));
+                    if (j > 0) colorSwaping(document.getElementById(i + "" + (j - 1)));
+                    if (j < tiles[0].length - 1) colorSwaping(document.getElementById(i + "" + (j + 1)));
 
-                steps++;
-                step.textContent = steps;
-                tdElements.forEach(td => {
-                    if (!td.classList.contains("gray"))
-                        allOff = false;
-                });
+                    steps++;
+                    step.textContent = steps;
+                    tdElements.forEach(td => {
+                        if (!td.classList.contains("gray"))
+                            allOff = false;
+                    });
 
-                if (allOff) {
-                    clearInterval(timer);
-                    document.getElementById("modalArea").style.display = "block";
-                    if(sec < 10)
-                    document.getElementById("modalText").innerHTML += "<p>Steps: " + steps + "/" + target + "</p><p>Time: " + min + ":0" + sec + "</p>";
-                    else
-                    document.getElementById("modalText").innerHTML += "<p>Steps: " + steps + "/" + target + "</p><p>Time: " + min + ":" + sec + "</p>";
+                    if (allOff) {
+                        clearInterval(timer);
+                        gameOver = true;
+                        document.getElementById("modalArea").style.display = "block";
+                        if (sec < 10)
+                            document.getElementById("modalText").innerHTML += "<p>Steps: " + steps + "/" + target + "</p><p>Time: " + min + ":0" + sec + "</p>";
+                        else
+                            document.getElementById("modalText").innerHTML += "<p>Steps: " + steps + "/" + target + "</p><p>Time: " + min + ":" + sec + "</p>";
+                    }
                 }
             });
         });
@@ -87,15 +91,14 @@ document.addEventListener("DOMContentLoaded", function () {
                 sec = 0;
                 min++;
             }
-            if(sec < 10)
-            timeLeftElement.textContent = min + ":0" + sec;
+            if (sec < 10)
+                timeLeftElement.textContent = min + ":0" + sec;
             else
-            timeLeftElement.textContent = min + ":" + sec;
+                timeLeftElement.textContent = min + ":" + sec;
 
         }, 1000);
     }
-}
-);
+});
 
 function tableCreating(tiles, target) {
     let html = "";
